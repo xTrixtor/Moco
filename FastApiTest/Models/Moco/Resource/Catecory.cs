c891265bde4e9197d16d6fbe9ac9a7a02c59ddb4
Nico Böhner <nico.boehner1999@gmail.com>
2023-10-27 13:42:55 +0200

    wip

---
+++ FastApiTest/Models/Moco/Resource/Catecory.cs
@@ -0,0 +1,19 @@
+﻿namespace MocoApi.Models.Moco.Resource
+{
+    public enum Catecory
+    {
+        Sonstiges,
+        Vertraglich,
+        Versicherung,
+        Essen,
+        Haushalt,
+        Aktivität,
+        Ausgehen,
+        Investieren,
+        Tanken,
+        Kredit,
+        Wohnen,
+        Entertainment,
+        Luxus
+    }
+}
